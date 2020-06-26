b93e15eff80d8a66f720c32d596d72edffc52a70
Santiago Montoya <xantiagoma@gmail.com>
2020-06-26 00:41:14 -0500

    Hooks try

--- lib/screens/main.dart
+++ lib/screens/main.dart
@@ -1,10 +1,12 @@
 import 'package:dovy/general.dart';
 
-class MainScreen extends StatelessWidget {
+class MainScreen extends HookWidget {
   const MainScreen({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final reverse = useState(false);
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: context.theme.scaffoldBackgroundColor,
@@ -19,21 +21,29 @@ class MainScreen extends StatelessWidget {
       body: Column(
         children: <Widget>[
           Text(context.s.appDescription),
-          Container(
-            decoration: BoxDecoration(
-              color: context.theme.accentColor,
-              shape: BoxShape.circle,
-            ),
-            width: context.media.size.width,
-            height: context.media.size.width,
-            margin: EdgeInsets.all(context.media.size.width * 0.1),
-            child: Lottie.asset(
-              "assets/lottie/map-location.json",
-              fit: BoxFit.contain,
+          GestureDetector(
+            onTap: () {
+              reverse.value = !reverse.value;
+            },
+            child: Container(
+              decoration: BoxDecoration(
+                color: context.theme.accentColor,
+                shape: BoxShape.circle,
+              ),
+              width: context.media.size.width,
+              height: context.media.size.width,
+              margin: EdgeInsets.all(context.media.size.width * 0.1),
+              child: Lottie.asset(
+                "assets/lottie/map-location.json",
+                fit: BoxFit.contain,
+                animate: reverse.value,
+              ),
             ),
           ),
           FlatButton(
-            color: context.theme.primaryColor,
+            color: reverse.value
+                ? context.theme.primaryColor
+                : context.theme.accentColor,
             child: Text("Start"),
             onPressed: () {
               context.navigateTo(
